4ea9464928ca2dfa6a7309b3a6aaed6923d683f3
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-04 17:25:13 -0800

    include statements

--- src/chess/board.hpp
+++ src/chess/board.hpp
@@ -16,3 +16,13 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
+
+#pragma once
+
+#include <vector>
+#include <string>
+#include <map>
+
+
+class Board {
+};
