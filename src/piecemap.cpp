bcf5867deb450765f66f018aa580c9c42c8517c9
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-05 23:47:36 -0800

    Moved piece map classes to separate files.

---
+++ src/piecemap.cpp
@@ -0,0 +1,100 @@
+//
+//  Megalodon
+//  UCI chess engine
+//  Copyright Patrick Huang and Arjun Sahlot 2021
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "chess/constants.hpp"
+#include "piecemap.hpp"
+
+using std::cin;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+
+PieceMap::PieceMap() {
+    _weights = {};
+    for (auto y = 0; y < 8; y++) {
+        vector<float> row;
+        for (auto x = 0; x < 8; x++) row.push_back(0);
+        _weights.push_back(row);
+    }
+}
+
+PieceMap::PieceMap(string fname) {
+    set_weights(fname);
+}
+
+void PieceMap::set_weights(string fname) {
+    cout << "info string loading piece map from " << fname << "..." << std::flush;
+
+    std::ifstream file(fname);
+    _weights = {};
+    for (auto y = 0; y < 8; y++) {
+        vector<float> row;
+        for (auto x = 0; x < 8; x++) {
+            float weight;
+            file >> weight;
+            row.push_back(weight);
+        }
+        _weights.push_back(row);
+    }
+
+    cout << "finished" << endl;
+}
+
+void PieceMap::normalize(int bound) {
+    int max_val = 0;
+    for (auto row: _weights) {
+        for (auto weight: row) {
+            if (weight > max_val) max_val = weight;
+        }
+    }
+
+    for (auto y = 0; y < 8; y++) {
+        for (auto x = 0; x < 8; x++) {
+            _weights[y][x] *= (bound/max_val); 
+        }
+    }
+}
+
+float PieceMap::eval(int x, int y) {
+    return _weights[y][x];
+}
+
+string PieceMap::as_string() {
+    string str;
+    str += " " + BOARD_OUTROW + "\n";
+    for (auto i = 0; i < 8; i++) {
+        str += BOARD_OUTCOL;
+        for (auto piece: _weights[i]) {
+            str += std::to_string(piece) + BOARD_OUTCOL;
+        }
+        str += std::to_string(8-i) + "\n";
+        str += " " + BOARD_OUTROW + "\n";
+    }
+    str += "   ";
+    for (auto i: "abcdefgh") str += string(1, i) + "   ";
+
+    return str;
+}
