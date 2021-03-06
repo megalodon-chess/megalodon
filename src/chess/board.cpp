6b332fdc67af0b80fccd6f4bad75e68b7b1d1d7e
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-06 11:38:46 -0800

    filter legal movse

--- src/chess/board.cpp
+++ src/chess/board.cpp
@@ -235,11 +235,11 @@ vector<Move> Board::get_all_legal_moves() {
             if (piece_color(piece) != _turn) continue;
             vector<Move> new_moves;
             switch (piece) {
-                case WP: case BP: new_moves = pawn_moves({row, col}); break; 
-                case WN: case BN: new_moves = knight_moves({row, col}); break;
-                case WB: case BB: new_moves = bishop_moves({row, col}); break;
-                case WQ: case BQ: new_moves = queen_moves({row, col}); break;
-                case WK: case BK: new_moves = king_moves({row, col}); break;
+                case WP: case BP: new_moves = _filter_moves(pawn_moves({row, col})); break; 
+                case WN: case BN: new_moves = _filter_moves(knight_moves({row, col})); break;
+                case WB: case BB: new_moves = _filter_moves(bishop_moves({row, col})); break;
+                case WQ: case BQ: new_moves = _filter_moves(queen_moves({row, col})); break;
+                case WK: case BK: new_moves = _filter_moves(king_moves({row, col})); break;
             }
 
             moves.reserve(moves.size() + new_moves.size());  // reverse for performance
