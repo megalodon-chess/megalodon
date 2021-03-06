631ef4db9c7cb6ce60c9bf02672655c3f71326ab
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-06 10:56:07 -0800

    improve syntax

--- src/chess/board.cpp
+++ src/chess/board.cpp
@@ -205,28 +205,23 @@ vector<Move> Board::get_all_legal_moves() {
             if (piece_color(piece) != _turn) break;
             vector<Move> new_moves;
             switch (piece) {
-                case WP:
-                case BP:
+                case WP: case BP:
                     new_moves = pawn_moves({row, col});
                     moves.reserve(moves.size() + new_moves.size());  // reverse for performance
                     moves.insert(moves.end(), new_moves.begin(), new_moves.end()); break;
-                case WN:
-                case BN:
+                case WN: case BN:
                     new_moves = knight_moves({row, col});
                     moves.reserve(moves.size() + new_moves.size());  // reverse for performance
                     moves.insert(moves.end(), new_moves.begin(), new_moves.end()); break;
-                case WB:
-                case BB:
+                case WB: case BB:
                     new_moves = bishop_moves({row, col});
                     moves.reserve(moves.size() + new_moves.size());  // reverse for performance
                     moves.insert(moves.end(), new_moves.begin(), new_moves.end()); break;
-                case WQ:
-                case BQ:
+                case WQ: case BQ:
                     new_moves = queen_moves({row, col});
                     moves.reserve(moves.size() + new_moves.size());  // reverse for performance
                     moves.insert(moves.end(), new_moves.begin(), new_moves.end()); break;
-                case WK:
-                case BK:
+                case WK: case BK:
                     new_moves = king_moves({row, col});
                     moves.reserve(moves.size() + new_moves.size());  // reverse for performance
                     moves.insert(moves.end(), new_moves.begin(), new_moves.end()); break;
