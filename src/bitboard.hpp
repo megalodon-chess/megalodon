ebb991b086953fbc8f31a7b653f751e4c2236262
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-07 15:10:24 -0800

    Made starting position unsigned.

--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -29,19 +29,19 @@ using std::endl;
 using std::vector;
 using std::string;
 
-constexpr long long START_WP = 65280;
-constexpr long long START_WN = 66;
-constexpr long long START_WB = 36;
-constexpr long long START_WR = 129;
-constexpr long long START_WQ = 8;
-constexpr long long START_WK = 16;
+constexpr unsigned long long START_WP = 65280;
+constexpr unsigned long long START_WN = 66;
+constexpr unsigned long long START_WB = 36;
+constexpr unsigned long long START_WR = 129;
+constexpr unsigned long long START_WQ = 8;
+constexpr unsigned long long START_WK = 16;
 
-constexpr long long START_BP = 71776119061217280;
-constexpr long long START_BN = 4755801206503243776;
-constexpr long long START_BB = 2594073385365405696;
-constexpr long long START_BR = 9295429630892703744;
-constexpr long long START_BQ = 576460752303423488;
-constexpr long long START_BK = 1152921504606846976;
+constexpr unsigned long long START_BP = 71776119061217280;
+constexpr unsigned long long START_BN = 4755801206503243776;
+constexpr unsigned long long START_BB = 2594073385365405696;
+constexpr unsigned long long START_BR = 9295429630892703744;
+constexpr unsigned long long START_BQ = 576460752303423488;
+constexpr unsigned long long START_BK = 1152921504606846976;
 
 bool bit(long long, int);
 string repr(long long);
