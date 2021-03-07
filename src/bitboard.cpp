51983e39e348980ba7a037fcf2a6b679dad124dd
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-07 15:41:17 -0800

    Moved functions to namespace.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -29,58 +29,60 @@ using std::vector;
 using std::string;
 
 
-bool bit(long long board, int pos) {
-    return ((1LL << pos) & board) != 0;
-}
-
-long long set_bit(long long board, int pos, bool value) {
-    bool on = bit(board, pos);
-    if (value && !on) {
-        board += (1LL << pos);
-    } else if (!value && on) {
-        board -= (1LL << pos);
+namespace Bitboard {
+    bool bit(long long board, int pos) {
+        return ((1LL << pos) & board) != 0;
     }
-    return board;
-}
 
-long long push(long long board, vector<char> move) {
-    board = set_bit(board, move[0], false);
-    board = set_bit(board, move[1], true);
-    return board;
-}
-
-string repr(long long board) {
-    vector<string> rows;
-    string final = "";
-
-    for (auto y = 0; y < 8; y++) {
-        string row = "";
-        for (auto x = 0; x < 8; x++) {
-            row += bit(board, 8*y+x) ? "1" : ".";
-            row += " ";
+    long long set_bit(long long board, int pos, bool value) {
+        bool on = bit(board, pos);
+        if (value && !on) {
+            board += (1LL << pos);
+        } else if (!value && on) {
+            board -= (1LL << pos);
         }
-        rows.push_back(row);
+        return board;
     }
 
-    for (auto i = 7; i >= 0; i--) {
-        final += rows[i];
-        final += "\n";
+    long long push(long long board, vector<char> move) {
+        board = set_bit(board, move[0], false);
+        board = set_bit(board, move[1], true);
+        return board;
     }
 
-    return final;
-}
+    string board_str(long long board) {
+        vector<string> rows;
+        string final = "";
 
-vector<vector<char>> knight_moves(long long knight, long long same_col, long long diff_col) {
-    vector<vector<char>> moves;
+        for (auto y = 0; y < 8; y++) {
+            string row = "";
+            for (auto x = 0; x < 8; x++) {
+                row += bit(board, 8*y+x) ? "1" : ".";
+                row += " ";
+            }
+            rows.push_back(row);
+        }
 
-    for (char i = 0; i < 64; i++) {
-        if (bit(knight, i)) {
-            for (auto dir: DIR_N) {
-                char pos = i + 8*dir[0] + dir[1];
-                if (((1LL << pos) & same_col) == 0) moves.push_back({i, pos});
+        for (auto i = 7; i >= 0; i--) {
+            final += rows[i];
+            final += "\n";
+        }
+
+        return final;
+    }
+
+    vector<vector<char>> knight_moves(long long knight, long long same_col, long long diff_col) {
+        vector<vector<char>> moves;
+
+        for (char i = 0; i < 64; i++) {
+            if (bit(knight, i)) {
+                for (auto dir: DIR_N) {
+                    char pos = i + 8*dir[0] + dir[1];
+                    if (((1LL << pos) & same_col) == 0) moves.push_back({i, pos});
+                }
             }
         }
-    }
 
-    return moves;
+        return moves;
+    }
 }
--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -29,25 +29,27 @@ using std::endl;
 using std::vector;
 using std::string;
 
-constexpr unsigned long long START_WP = 65280;
-constexpr unsigned long long START_WN = 66;
-constexpr unsigned long long START_WB = 36;
-constexpr unsigned long long START_WR = 129;
-constexpr unsigned long long START_WQ = 8;
-constexpr unsigned long long START_WK = 16;
+namespace Bitboard {
+    constexpr unsigned long long START_WP = 65280;
+    constexpr unsigned long long START_WN = 66;
+    constexpr unsigned long long START_WB = 36;
+    constexpr unsigned long long START_WR = 129;
+    constexpr unsigned long long START_WQ = 8;
+    constexpr unsigned long long START_WK = 16;
 
-constexpr unsigned long long START_BP = 71776119061217280;
-constexpr unsigned long long START_BN = 4755801206503243776;
-constexpr unsigned long long START_BB = 2594073385365405696;
-constexpr unsigned long long START_BR = 9295429630892703744;
-constexpr unsigned long long START_BQ = 576460752303423488;
-constexpr unsigned long long START_BK = 1152921504606846976;
+    constexpr unsigned long long START_BP = 71776119061217280;
+    constexpr unsigned long long START_BN = 4755801206503243776;
+    constexpr unsigned long long START_BB = 2594073385365405696;
+    constexpr unsigned long long START_BR = 9295429630892703744;
+    constexpr unsigned long long START_BQ = 576460752303423488;
+    constexpr unsigned long long START_BK = 1152921504606846976;
 
-const vector<vector<int>> DIR_N = {{-2, 1}, {2, 1}, {-2, -1}, {2, -1}, {1, -2}, {-1, 2}, {-1, -2}, {1, 2}};
+    const vector<vector<int>> DIR_N = {{-2, 1}, {2, 1}, {-2, -1}, {2, -1}, {1, -2}, {-1, 2}, {-1, -2}, {1, 2}};
 
-bool bit(long long, int);
-long long set_bit(long long, int, bool);
-long long push(long long, vector<char>);
-string repr(long long);
+    bool bit(long long, int);
+    long long set_bit(long long, int, bool);
+    long long push(long long, vector<char>);
+    string board_str(long long);
 
-vector<vector<char>> knight_moves(long long knight, long long same_col, long long diff_col);
+    vector<vector<char>> knight_moves(long long knight, long long same_col, long long diff_col);
+}
