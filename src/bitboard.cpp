5d1a0c17e6b609203f770c9013b7e8e9d87cd061
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-07 18:06:47 -0800

    rearrange

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -97,24 +97,6 @@ namespace Bitboard {
         return moves;
     }
 
-    vector<vector<char>> rook_moves(long long board, long long same_col, long long diff_col) {
-        vector<vector<char>> moves;
-        for (char i = 0; i < 64; i++) {
-            if (bit(board, i)) {
-                for (auto dir: DIR_R) {
-                    for (auto dist = 1; dist < 8; dist++) {
-                        char pos = i + dist*8*dir[0] + dist*dir[1];
-                        auto shift = 1LL << pos;
-                        if ((shift & same_col) != 0) break;
-                        moves.push_back({i, pos});
-                        if ((shift & diff_col) != 0) break;
-                    }
-                }
-            }
-        }
-        return moves;
-    }
-
     vector<vector<char>> bishop_moves(long long board, long long same_col, long long diff_col) {
         vector<vector<char>> moves;
         for (char i = 0; i < 64; i++) {
@@ -133,6 +115,24 @@ namespace Bitboard {
         return moves;
     }
     
+    vector<vector<char>> rook_moves(long long board, long long same_col, long long diff_col) {
+        vector<vector<char>> moves;
+        for (char i = 0; i < 64; i++) {
+            if (bit(board, i)) {
+                for (auto dir: DIR_R) {
+                    for (auto dist = 1; dist < 8; dist++) {
+                        char pos = i + dist*8*dir[0] + dist*dir[1];
+                        auto shift = 1LL << pos;
+                        if ((shift & same_col) != 0) break;
+                        moves.push_back({i, pos});
+                        if ((shift & diff_col) != 0) break;
+                    }
+                }
+            }
+        }
+        return moves;
+    }
+
     vector<vector<char>> queen_moves(long long board, long long same_col, long long diff_col) {
         vector<vector<char>> moves;
         for (char i = 0; i < 64; i++) {
