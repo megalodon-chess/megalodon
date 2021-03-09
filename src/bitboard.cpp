6ffc603f10ded17b5a8f098221950df1a2074980
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-09 09:44:25 -0800

    Improve rook checks.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -237,7 +237,7 @@ namespace Bitboard {
                                 if (!(0 <= cx && cx < 8 && 0 <= cy && cy < 8)) break;
                                 const char loc = cy*8 + cx;
                                 if (bit(same_side, loc)) break;
-                                if ((1ULL << loc) & rooks != 0) board = set_bit(board, loc, true); num_atckers++; break;
+                                if (bit(rooks, loc)) board = set_bit(board, loc, true); num_atckers++; break;
                                 if (bit(pieces, loc)) break;
                             }
                         }
