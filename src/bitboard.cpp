251c9f8700bb3c3d1cf56b2e941ec02a772e7cb3
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-12 17:18:51 -0800

    Add 2 way support while check.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -679,12 +679,23 @@ namespace Bitboard {
                         // Block
                         y = i/8;
                         const char speed = (y == (pos.turn ? 1 : 6)) ? 2 : 1;  // If white check rank 6 else rank 1 if on that rank 2 else 1
-                        for (auto cy = y + 1; cy < y + speed + 1; cy++) {
-                            const char loc = cy*8 + x;
-                            if (bit(ALL, loc)) break;
-                            if ((1ULL << loc) & block_mask) {
-                                moves.push_back(Move(i, loc));
-                                break;
+                        if (pos.turn) {
+                            for (auto cy = y + 1; cy < y + speed + 1; cy++) {
+                                const char loc = cy*8 + x;
+                                if (bit(ALL, loc)) break;
+                                if ((1ULL << loc) & block_mask) {
+                                    moves.push_back(Move(i, loc));
+                                    break;
+                                }
+                            }
+                        } else {
+                            for (auto cy = y - 1; cy > y - speed - 1; cy--) {
+                                const char loc = cy*8 + x;
+                                if (bit(ALL, loc)) break;
+                                if ((1ULL << loc) & block_mask) {
+                                    moves.push_back(Move(i, loc));
+                                    break;
+                                }
                             }
                         }
                     } else if (bit(CN, i)) {
