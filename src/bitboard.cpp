cc343a2d7c75a1213abe49a1dd7dcc34ce68073f
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-14 15:50:14 -0700

    Bug fix.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -593,8 +593,8 @@ namespace Bitboard {
 
             char dx = check_x - kx, dy = check_y - ky;
             if (!(std::find(DIR_K.begin(), DIR_K.end(), vector<char>({dx, dy})) != DIR_K.end())) {
-                dx /= abs(dx);
-                dy /= abs(dy);
+                if (dx) dx /= abs(dx);
+                if (dy) dy /= abs(dy);
                 char cx = kx, cy = ky;   // Current (x, y)
                 while (true) {
                     cx += dx;
