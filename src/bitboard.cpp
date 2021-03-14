52172037bff2faea5049d2070307469dba0e39c2
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-14 15:47:35 -0700

    Simplify king moves.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -527,7 +527,7 @@ namespace Bitboard {
     vector<Move> king_moves(U64 king, U64 same, U64 attacks) {
         // Pass in attacks from opponent.
         vector<Move> moves;
-        
+
         tuple<char, char> k_pos = first_bit(king);
 
         const char x = get<0>(k_pos)%8, y = get<1>(k_pos)/8;
@@ -535,7 +535,7 @@ namespace Bitboard {
             const char kx = x+dir[0], ky = y+dir[1];
             if (0 <= kx && kx < 8 && 0 <= ky && ky < 8) {
                 const char loc = ky*8 + kx;
-                if (((1ULL << loc) & attacks) == EMPTY && !bit(same, loc)) moves.push_back(Move(y*8 + x, loc));
+                if (!bit(attacks, loc) && !bit(same, loc)) moves.push_back(Move(y*8 + x, loc));
             }
         }
 
