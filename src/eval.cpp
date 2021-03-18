bec6ada565a36092858f46a8e0abb966488418f1
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-17 23:48:25 -0700

    Extra check to prevent 0 to 1.

--- src/eval.cpp
+++ src/eval.cpp
@@ -137,8 +137,8 @@ float rooks(Options& options, U64 rooks) {
         }
     }
     for (auto i = 0; i < 8; i++) {
-        score += std::pow(files[i], 2)/2;  // ((Number of rooks on file)^2)/2
-        score += std::pow(ranks[i], 2)/2;  // ((Number of rooks on rank)^2)/2
+        if (files[i]) score += std::pow(files[i], 2)/2;  // ((Number of rooks on file)^2)/2
+        if (ranks[i]) score += std::pow(ranks[i], 2)/2;  // ((Number of rooks on rank)^2)/2
         // todo add more score when rook in middle files or when rook on good ranks (7, 1)
     }
 }
