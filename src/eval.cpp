d0194146cf4dba69e7690ed8d4722fd2419d10ab
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-17 23:47:43 -0700

    Fix comment.

--- src/eval.cpp
+++ src/eval.cpp
@@ -138,7 +138,7 @@ float rooks(Options& options, U64 rooks) {
     }
     for (auto i = 0; i < 8; i++) {
         score += std::pow(files[i], 2)/2;  // ((Number of rooks on file)^2)/2
-        score += std::pow(ranks[i], 2)/2;  // ((Number of rooks on file)^2)/2
+        score += std::pow(ranks[i], 2)/2;  // ((Number of rooks on rank)^2)/2
         // todo add more score when rook in middle files or when rook on good ranks (7, 1)
     }
 }
