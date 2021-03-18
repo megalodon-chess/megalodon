7c9a8c7803ef86aa7da6429d75fd3cb90d738f6e
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-17 21:31:47 -0700

    Changed values.

--- src/eval.cpp
+++ src/eval.cpp
@@ -185,5 +185,5 @@ float eval(Options& options, Position pos, bool moves_exist, int depth) {
 
     const float cent = center_control(options, pos, stage);
 
-    return mat + 0.65*cent + 0.75*(wking-bking) + (wpawn-bpawn) + 0.6*(wknight-bknight);
+    return mat + 0.35*cent + 0.75*(wking-bking) + (wpawn-bpawn) + 0.6*(wknight-bknight);
 }
