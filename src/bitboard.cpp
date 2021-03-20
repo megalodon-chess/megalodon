c3565cec7dce5e6c4e6215efd573fdb8e3679421
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-03-19 22:49:39 -0700

    Store num moves.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -881,17 +881,18 @@ namespace Bitboard {
     }
 
     vector<Move> order_moves(Position pos, vector<Move> moves) {
-        vector<tuple<Move, int>> evaluated_moves(moves.size()); 
+        const char num_moves = moves.size();
+        vector<tuple<Move, int>> evaluated_moves(num_moves); 
 
-        for (char i = 0; i < moves.size(); i++) {
+        for (char i = 0; i < num_moves; i++) {
             evaluated_moves[i] = tuple<Move, int>(moves[i], quick_eval(pos, moves[i]));
         }
 
         std::sort(evaluated_moves.begin(), evaluated_moves.end(), [](tuple<Move, int> a, tuple<Move, int> b){return get<1>(a) < get<1>(b);});
 
-        vector<Move> final_moves(moves.size());
+        vector<Move> final_moves(num_moves);
 
-        for (char i = 0; i < moves.size(); i++) final_moves[i] = get<0>(evaluated_moves[i]);
+        for (char i = 0; i < num_moves; i++) final_moves[i] = get<0>(evaluated_moves[i]);
 
         return moves; 
     }
