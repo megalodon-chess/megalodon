4249d2ca9014200e884c11c35a93b04d07ee1d24
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-27 13:15:18 -0700

    Access hash with namespace.

--- src/perft.cpp
+++ src/perft.cpp
@@ -48,7 +48,7 @@ namespace Perft {
 
     double hash_perft(const Position& pos, const int& knodes) {
         double start = get_time();
-        for (auto i = 0; i < knodes*1000; i++) hash(pos);
+        for (auto i = 0; i < knodes*1000; i++) Hash::hash(pos);
         return get_time() - start;
     }
 }
