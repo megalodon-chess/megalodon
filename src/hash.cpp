5de0bd1b2e52cd2dd2cbbf211976ed7157f0b8de
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-27 20:06:13 -0700

    Improved hash algorithm.

--- src/hash.cpp
+++ src/hash.cpp
@@ -33,25 +33,23 @@ using std::string;
 
 namespace Hash {
     U64 hash(const Position& pos) {
-        const unsigned char c1 = (pos.wp%43) + (pos.bp%137) + ((pos.wn|pos.wb|pos.bn|pos.bb|pos.wr|pos.br)%44) + 31*pos.turn;
-        const unsigned char c2 = ((pos.wn<<c1) & 255ULL) + RANDC[c1];
-        const unsigned char c3 = ((pos.bp<<c2) & 255ULL) + RANDC[c2];
-        const unsigned char c4 = ((pos.wr<<c3) & 255ULL) + RANDC[c3];
-        const U64 u1 = ((pos.wp^pos.bp) << (RANDC[c1]&63ULL)) + (S2<<c1);
-        const U64 u2 = ((pos.bp^pos.wn) << (RANDC[c2]&63ULL)) + (S3<<c2);
-        const U64 u3 = ((pos.wn^pos.bq) << (RANDC[c3]&63ULL)) + (S5<<c3);
-        const U64 u4 = ((pos.wk^pos.bn) << (RANDC[c4]&63ULL)) + (S7<<c4);
+        const unsigned char c1 = pos.turn ? 5 : 41;
+        const unsigned char c2 = pos.turn ? 23 : 1;
+        const unsigned char c3 = pos.turn ? 29 : 54;
+        const unsigned char c4 = pos.turn ? 11 : 36;
+        const unsigned short s1 = RANDS[pos.ep_square + c1];
+        const unsigned short s2 = pos.turn ? S3 : S7;
+        const unsigned short s3 = pos.turn ? S5 : S2;
+        const U64 u1 = (pos.wp ^ pos.bp) << RANDC[c1];
+        const U64 u2 = (pos.bp ^ pos.wn) << RANDC[c2];
+        const U64 u3 = (pos.wn ^ pos.bq) << RANDC[c3];
+        const U64 u4 = (pos.wk ^ pos.bn) << RANDC[c4];
         const U64 u5 = ((U64)c1) << abs(c2-c3);
         const U64 u6 = pos.wn | pos.bb | pos.wr | pos.br;
         const U64 u7 =  ((S2<<RANDC[c1]) + u4) ^ ((S3<<RANDC[c3]) + u1) + pos.wn - pos.br + pos.wp - pos.wk;
         const U64 u8 =  ((S7<<RANDC[c4]) + u6) | ((S5<<RANDC[c3]) + u2) + pos.bb - pos.wk + pos.bp - pos.wn;
         const U64 u9 =  ((S5<<RANDC[c2]) + u1) ^ ((S7<<RANDC[c1]) + u2) + pos.wb - pos.bk + pos.wn - pos.wp;
         const U64 u10 = ((S2<<RANDC[c2]) + u3) | ((S3<<RANDC[c4]) + u6) + pos.bq - pos.wq + pos.bn - pos.bp;
-        return (
-            (u7>>c1) + (u8>>c2) + (u9>>c3) + (u10>>c4)
-            - ((u1<<RANDC[c3]) + (u2<<RANDC[c1]) + (u3<<RANDC[c4]) + (u4<<RANDC[c2]))
-            + (RANDS[c1]*RANDS[c4] + RANDS[c2]*RANDS[c3] + RANDS[c2]*RANDS[c4] + RANDS[c1]*RANDS[c3])
-            - ((S2<<c4) + (S3<<c1) + (S5<<c3) + (S7<<c2))
-        );
+        return (u7>>c1) + (u8>>c2) + (u9>>c3) + (u10>>c4);
     }
 }
