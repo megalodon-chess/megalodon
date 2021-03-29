a7972e56fe252d4f0ec388d62df9d17b7c615249
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-28 19:03:39 -0700

    Added push performance test.

--- src/perft.hpp
+++ src/perft.hpp
@@ -34,4 +34,5 @@ namespace Perft {
     int movegen(const Position&, const int&);
     double hash_perft(const Position&, const int&);
     double eval_perft(const Options&, const Position&, const int&);
+    double push_perft(const Position&, const int&);
 }
