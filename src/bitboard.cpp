573e4fcc9aa6d357aea7a411dc28c7b09f8c017d
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-04-06 21:03:02 -0700

    Added todo comment.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -730,6 +730,7 @@ namespace Bitboard {
                         }
                     }
                     // Capture
+                    // TODO a bug here, fen = 8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1
                     U64 new_capture = capture_mask;
                     if (pos.ep) {
                         const char start = pos.turn ? 4 : 3;
