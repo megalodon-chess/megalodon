7a81776c43827708aa09ca6595761b9fcf46a952
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-04-07 23:03:01 -0700

    Added benchmark. Tests 10 positions and prints results. Inspired by Stockfish.
    Bench: 2196682 nodes.

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,8 @@
 #include "bitboard.hpp"
 #include "hash.hpp"
 #include "eval.hpp"
+#include "search.hpp"
+#include "utils.hpp"
 
 #define VERSION  "0.4.0"
 
@@ -48,9 +50,50 @@ void init() {
 }
 
 
+void bench() {
+    const Options options;
+    const int num_pos = 10;
+    const int depth = 4;
+    const string fens[num_pos] = {
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r2q1rk1/ppp2pp1/2np1n1p/2b1p3/2B1P1b1/2NPBN2/PPPQ1PPP/R3R1K1 b Qq - 0 1",
+        "6k1/5ppp/8/8/8/8/5PPP/6K1 w - - 0 1",
+        "8/8/NnNnNnNn/8/8/8/8/2k2K2 b - - 0 1",
+        "7k/p6P/p6P/p6P/p6P/p6P/p6P/K7 w - - 0 1",
+        "8/8/8/8/8/2k5/8/1QK5 w - - 0 1",
+        "4k3/p7/Pp6/1Pp1p3/2PpPp1p/3P1PpP/6P1/3K4 w - - 0 1",
+        "8/1k6/3K4/2P5/8/8/8/8 w - - 0 1",
+        "r2q1rk1/pbp1bpp1/1pn1pn1p/3p4/2PP3B/1PN1PN2/P3BPPP/R2Q1RK1 b Qq - 0 1",
+        "2k4r/1pp5/p7/2p1N3/3n1P2/7P/6P1/R5K1 b - - 0 1",
+    };
+
+    U64 nodes = 0;
+    const double start = get_time();
+
+    for (auto i = 0; i < num_pos; i++) {
+        cout << "Position " << i+1 << " of " << num_pos << endl;
+        cout << "Fen: " << fens[i] << endl;
+
+        bool searching = true;
+        const Position pos = Bitboard::parse_fen(fens[i]);
+        const SearchInfo result = Search::search(options, pos, depth, 10000, false, searching);
+        nodes += result.nodes;
+    }
+
+    const double elapse = get_time() - start;
+    const int nps = nodes / elapse;
+
+    cout << "\nBenchmark results:" << endl;
+    cout << "Nodes: " << nodes << endl;
+    cout << "NPS: " << nps << endl;
+    cout << "Time: " << elapse << " seconds" << endl;
+}
+
+
 int main(const int argc, const char* argv[]) {
     if (argc >= 2) {
-        if (argv[1] == string("--version")) cout << VERSION << endl;
+        if      (argv[1] == string("--version")) cout << VERSION << endl;
+        else if (argv[1] == string("bench")) bench();
     } else {
         srand(1234);
         init();
