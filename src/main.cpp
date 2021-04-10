d323febf00d8ccd0c155d8371cb3a898316f648c
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-04-09 18:57:13 -0700

    Added more bench positions.

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ void init() {
 
 void bench() {
     const Options options;
-    constexpr char num_pos = 10;
+    constexpr char num_pos = 20;
     constexpr char depth = 4;
     const string fens[num_pos] = {
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
@@ -66,6 +66,16 @@ void bench() {
         "8/1k6/3K4/2P5/8/8/8/8 w - - 0 1",
         "r2q1rk1/pbp1bpp1/1pn1pn1p/3p4/2PP3B/1PN1PN2/P3BPPP/R2Q1RK1 b Qq - 0 1",
         "2k4r/1pp5/p7/2p1N3/3n1P2/7P/6P1/R5K1 b - - 0 1",
+        "7k/p7/8/8/8/8/P7/7K b - - 0 1",
+        "8/8/8/3b4/8/3Bk3/1BK2b2/8 b - - 0 1",
+        "8/3qPK2/8/8/8/4k3/8/8 w - - 0 1",
+        "8/7P/5K2/8/8/4k3/p7/8 w - - 0 1",
+        "r5k1/2p2ppp/pp2n3/8/P7/1B1P3P/1PP2PP1/3R1RK1 b - - 0 1",
+        "4k3/pppppppp/8/8/8/8/PPPPPPPP/4K3 b - - 0 1",
+        "r1r1r1r1/k7/8/8/8/8/1K6/1R1R1R1R w - - 0 1",
+        "K7/8/8/3P4/4p3/8/8/7k b - - 0 1",
+        "8/kpP3p1/p2n2P1/3Q3P/5q2/P7/KP6/8 w - - 0 1",
+        "8/8/8/8/8/k7/8/K7 w - - 0 1",
     };
 
     U64 nodes = 0;
