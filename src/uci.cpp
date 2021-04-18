7c8823f81e893e7ec926742154cdb28cdff878ce
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-04-18 16:07:19 -0700

    Add list of uci variants.

--- src/uci.cpp
+++ src/uci.cpp
@@ -20,6 +20,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <thread>
 #include "utils.hpp"
 #include "bitboard.hpp"
@@ -219,7 +220,14 @@ int loop() {
                 options.set_hash();
             }
 
-            else if (name == "UCI_Variant")    options.UCI_Variant    = value;
+            else if (name == "UCI_Variant") {
+                if (std::find(options.variants.begin(), options.variants.end(), value) != options.variants.end()) {
+                    cout << "Megalodon does not play " << value;
+                }
+                else {
+                    options.UCI_Variant = value;
+                }
+            }
 
             else if (name == "EvalMaterial")   options.EvalMaterial   = std::stof(value)/100;
             else if (name == "EvalPawnStruct") options.EvalPawnStruct = std::stof(value)/100;
